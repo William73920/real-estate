fa009f530c068ca6fba872c95d43d03473746918
Will <rrozario958@gmail.com>
2024-03-28 01:44:58 +0530

    completed show listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -28,6 +28,8 @@ const Profile = () => {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
 
   const handleFileUpload = (file) => {
@@ -130,6 +132,24 @@ const Profile = () => {
     }
   };
 
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/listings/${user?._id}`, {
+        method: "GET",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(data.message);
+        return;
+      }
+
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -223,6 +243,49 @@ const Profile = () => {
       <p className="text-green-500 text-center mt-5">
         {updateSuccess && "Profile updated successfully"}
       </p>
+      <button
+        onClick={handleShowListings}
+        className="text-green-700 text-center w-full"
+      >
+        Show Listings
+      </button>
+      <p className="text-red-500 text-center mt-5">
+        {showListingsError ? showListingsError : ""}
+      </p>
+
+      {userListings && userListings?.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h2 className="text-2xl font-semibold text-center mt-7">
+            Your Listings
+          </h2>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing-img"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+
+              <Link
+                className="flex-1 text-slate-700 font-semibold  hover:underline truncate"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className="flex flex-col items-center">
+                <button className="text-red-700 uppercase">Delete</button>
+                <button className="text-blue-700 uppercase ml-3">Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
